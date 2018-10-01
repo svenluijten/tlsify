a7773820cdc5c43e29e1fe0c7b90ad60849a945d
Sven Luijten <svenluijten@users.noreply.github.com>
2018-10-01 23:52:33 +0200

    code styling

--- tests/Feature/CrawlDirectoryTest.php
+++ tests/Feature/CrawlDirectoryTest.php
@@ -3,21 +3,24 @@
 namespace Sven\Tlsify\Tests\Feature;
 
 use Sven\Tlsify\DirectoryCrawler;
-use Sven\Tlsify\Tests\TestCase;
 use Sven\Tlsify\Regex;
+use Sven\Tlsify\Tests\TestCase;
 
 class CrawlDirectoryTest extends TestCase
 {
     /** @test */
     public function it_finds_files_with_http_urls_in_them()
     {
-        $path = $this->path(__DIR__ . '/__stubs__/insecure_and_secure');
+        $path = $this->path(__DIR__.'/__stubs__/insecure_and_secure');
 
         $files = DirectoryCrawler::find(Regex::HTTP_URL)->in($path);
 
         $this->assertInternalType('array', $files);
         $this->assertCount(1, $files);
-        $this->assertEquals($this->path(__DIR__.'/__stubs__/insecure_and_secure/insecure.txt'), reset($files)->getPathname());
+        $this->assertEquals(
+            $this->path(__DIR__.'/__stubs__/insecure_and_secure/insecure.txt'),
+            reset($files)->getPathname()
+        );
     }
 
     /** @test */
