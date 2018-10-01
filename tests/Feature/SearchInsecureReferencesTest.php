bbed687fcf593cb4657a183d2ecc8154a8389bec
Sven Luijten <svenluijten@users.noreply.github.com>
2018-10-01 22:25:04 +0200

    add 'path' helper in tests to normalize directory separators

--- tests/Feature/SearchInsecureReferencesTest.php
+++ tests/Feature/SearchInsecureReferencesTest.php
@@ -6,23 +6,25 @@ use Sven\Tlsify\Tests\TestCase;
 use Sven\Tlsify\Tlsify;
 use Symfony\Component\Finder\Finder;
 
-class SearchInsecureReferencesTest extends TestCase {
+class SearchInsecureReferencesTest extends TestCase
+{
     /** @test */
-    public function it_finds_files_with_http_urls_in_them() {
-        $finder = Finder::create()->in(__DIR__.DIRECTORY_SEPARATOR.'__stubs__'.DIRECTORY_SEPARATOR.'insecure_and_secure');
+    public function it_finds_files_with_http_urls_in_them()
+    {
+        $finder = Finder::create()->in($this->path(__DIR__.'/__stubs__/insecure_and_secure'));
 
-        /** @var \Symfony\Component\Finder\SplFileInfo[] $urls */
+        /** @var array|\Symfony\Component\Finder\SplFileInfo[] $urls */
         $urls = Tlsify::make($finder)->insecureUrls();
 
         $this->assertInternalType('array', $urls);
         $this->assertCount(1, $urls);
-        $this->assertEquals(__DIR__.DIRECTORY_SEPARATOR.'__stubs__'.DIRECTORY_SEPARATOR.'insecure_and_secure'.DIRECTORY_SEPARATOR.'insecure.txt', reset($urls)->getPathname());
+        $this->assertEquals($this->path(__DIR__.'/__stubs__/insecure_and_secure/insecure.txt'), reset($urls)->getPathname());
     }
 
     /** @test */
     public function it_can_return_an_iterator()
     {
-        $finder = Finder::create()->in(__DIR__.DIRECTORY_SEPARATOR.'__stubs__'.DIRECTORY_SEPARATOR.'insecure_and_secure');
+        $finder = Finder::create()->in($this->path(__DIR__.'/__stubs__/insecure_and_secure'));
 
         $this->assertInternalType('iterable', Tlsify::make($finder)->insecureUrls(true));
     }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,8 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function path(string $path): string
+    {
+        return str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
+    }
 }
